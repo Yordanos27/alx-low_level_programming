31305ae5ec6d96ac609de2af6a0c58b9c1bb1e54
Yordanos27 <yordanosabebe1325@gmail.com>
2022-12-08 18:54:15 -0800

    lkop

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - Entry point 
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %D byte(s)
+	
