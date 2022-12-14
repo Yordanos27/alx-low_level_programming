86a93f3c7f22e3714e5f77f37debeb0b155acfd0
Yordanos27 <yordanosabebe1325@gmail.com>
2022-12-14 12:07:39 -0800

    11-print_to_98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check description
+ * @a: AN integer a
+ * @b: AN integer b
+ * Description: Function that adds two integers and returns the result
+ *Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
