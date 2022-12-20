e83423075c296f9833ddfd453488c3da82659db2
Yordanos27 <yordanosabebe1325@gmail.com>
2022-12-19 18:27:37 -0800

    strcpy(scripthaha)

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy - a function that copies the string pointed to by src,
+ * including the terminationg null byte (\0),
+ * to the buffer pointed to by dest.
+ *
+ * @dest: A pointer to destination of string
+ * @src: A pointer to source string to copy from
+ *
+ * Return: pointer to des
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
+}
