f6e5e7d4e5b2263d503fa99bc6febcd2b9b3ac5f
Yordanos27 <yordanosabebe1325@gmail.com>
2022-12-22 16:58:53 -0800

    capitalizes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+int chech_separators(char c);
+
+/**
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to apitalize letters
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+
+		if (check_separators(s[i]) && (s[i = 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
+	}
+
+	return (s);
+}
+
+/**
+ * check_seperators - Separators of word: space, tabulation, new line,
+ * ,, ;m ., !, ?, ", ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seerator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
+}
