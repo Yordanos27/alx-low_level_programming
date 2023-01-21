1a9130a83326a6bbf7d94ce7f2b1d6e467765b78
Yordanos27 <yordanosabebe1325@gmail.com>
2023-01-21 09:16:12 -0800

    searches for integer correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,30 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: array to search in
+ * int_index - a function that searches for an integer.
+ * @array: A pointer to array
  * @size: size of the array
- * @cmp: pointer to the comparing function
- *
- * Return: index of the first element for which
- * the cmp function does not return 0, or -1 if no match is found
- * or size is negative
+ * @cmp: cmp(function) to compare the elements throw array
+ * Return: The index of the first element for which the cmp function
+ * does not return0; -1 if no elements matches or size <= 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (array && cmp)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i] != 0)
-					return (i);
-					}
-					}
-					return (-1)
-					}
+	if (cmp == NULL || array == NULL)
+		return (-1);
 
+	if (size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	if (i == size)
+		return (-1);
+
+	return (-1);
+}
