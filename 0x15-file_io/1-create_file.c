78db14b51c2f4650d36c7e6805af7493403d31e9
Yordanos27 <yordanosabebe1325@gmail.com>
2023-02-06 11:11:42 -0800

    ok

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fdo < 0)
 		return (-1);
 
-	fdw + write(fdo, text_content, len);
+	fdw = write(fdo, text_content, len);
 	close(fdo);
 	if (fdw < 0)
 		return (-1);
